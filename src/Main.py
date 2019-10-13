72f551ba7d1810f90243b0d23d06eca4c83b6a54
dh742ob <hreskodavid57@gmail.com>
2019-10-13 22:00:14 +0200

    probably final version

--- src/Main.py
+++ src/Main.py
@@ -2,8 +2,11 @@ from src.parsers.CrepcParser import CrepcParser
 from src.parsers.EpcParser import EpcParser
 
 if __name__ == '__main__':
+
+    # this will download base data
     epc_parser = EpcParser()
     epc_parser.load_workplace_records()
 
+    # this will download keywords
     #crepc_parser = CrepcParser()
     #crepc_parser.get_keywords()
