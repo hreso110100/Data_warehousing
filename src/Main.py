56020c3f14aa52744c611a2914ed07c833297e76
dh742ob <hreskodavid57@gmail.com>
2019-10-14 11:01:41 +0200

    keywords for rektorat

--- src/Main.py
+++ src/Main.py
@@ -2,11 +2,10 @@ from src.parsers.CrepcParser import CrepcParser
 from src.parsers.EpcParser import EpcParser
 
 if __name__ == '__main__':
-
     # this will download base data
-    epc_parser = EpcParser()
-    epc_parser.load_workplace_records()
+    # epc_parser = EpcParser()
+    # epc_parser.load_workplace_records()
 
     # this will download keywords
-    #crepc_parser = CrepcParser()
-    #crepc_parser.get_keywords()
+    crepc_parser = CrepcParser()
+    crepc_parser.get_keywords()
