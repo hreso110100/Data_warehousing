58d522b488430e6640ed8826798975b3a7c11261
michalpirchala <michal.pirchala@gmail.com>
2019-10-27 21:12:53 +0100

    DB schema, table for quotes

--- dbSchema/TSSU.sql
+++ dbSchema/TSSU.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Hostiteľ: localhost
--- Čas generovania: Ne 20.Okt 2019, 20:41
+-- Čas generovania: Ne 27.Okt 2019, 21:12
 -- Verzia serveru: 5.5.62-0+deb8u1
 -- Verzia PHP: 5.6.40-0+deb8u1
 
@@ -39,9 +39,7 @@ CREATE TABLE IF NOT EXISTS `authors` (
 
 CREATE TABLE IF NOT EXISTS `epcs` (
   `id` int(11) NOT NULL,
-  `epc_id` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
   `title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `title2` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `edition` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `publisher` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `numberOfPages` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -50,8 +48,7 @@ CREATE TABLE IF NOT EXISTS `epcs` (
   `isbn` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
   `issn` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
   `epc_cat` char(3) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `workplace` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `inserted_as_quote` tinyint(1) NOT NULL DEFAULT '0'
+  `workplace` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -82,6 +79,18 @@ CREATE TABLE IF NOT EXISTS `epcs_keywords` (
 -- --------------------------------------------------------
 
 --
+-- Štruktúra tabuľky pre tabuľku `epcs_quotes`
+--
+
+CREATE TABLE IF NOT EXISTS `epcs_quotes` (
+`id` int(11) NOT NULL,
+  `epc_id` int(11) NOT NULL,
+  `quote_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Štruktúra tabuľky pre tabuľku `keywords`
 --
 
@@ -97,8 +106,8 @@ CREATE TABLE IF NOT EXISTS `keywords` (
 --
 
 CREATE TABLE IF NOT EXISTS `quotes` (
-  `quoted` int(11) NOT NULL,
-  `quoted_in` int(11) NOT NULL
+`id` int(11) NOT NULL,
+  `quote` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -130,6 +139,12 @@ ALTER TABLE `epcs_keywords`
  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `epc_id_2` (`epc_id`,`keyword_id`), ADD KEY `epc_id` (`epc_id`), ADD KEY `keyword_id` (`keyword_id`);
 
 --
+-- Indexes for table `epcs_quotes`
+--
+ALTER TABLE `epcs_quotes`
+ ADD PRIMARY KEY (`id`), ADD KEY `epc_id` (`epc_id`), ADD KEY `quote_id` (`quote_id`);
+
+--
 -- Indexes for table `keywords`
 --
 ALTER TABLE `keywords`
@@ -139,7 +154,7 @@ ALTER TABLE `keywords`
 -- Indexes for table `quotes`
 --
 ALTER TABLE `quotes`
- ADD PRIMARY KEY (`quoted`,`quoted_in`), ADD KEY `quoted` (`quoted`), ADD KEY `quoted_in` (`quoted_in`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -156,9 +171,19 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `epcs_keywords`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `epcs_quotes`
+--
+ALTER TABLE `epcs_quotes`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `keywords`
 --
 ALTER TABLE `keywords`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `quotes`
+--
+ALTER TABLE `quotes`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Obmedzenie pre exportované tabuľky
@@ -179,11 +204,11 @@ ADD CONSTRAINT `epcs_keywords_ibfk_1` FOREIGN KEY (`epc_id`) REFERENCES `epcs` (
 ADD CONSTRAINT `epcs_keywords_ibfk_2` FOREIGN KEY (`keyword_id`) REFERENCES `keywords` (`id`);
 
 --
--- Obmedzenie pre tabuľku `quotes`
+-- Obmedzenie pre tabuľku `epcs_quotes`
 --
-ALTER TABLE `quotes`
-ADD CONSTRAINT `quoted` FOREIGN KEY (`quoted`) REFERENCES `epcs` (`id`),
-ADD CONSTRAINT `quoted_in` FOREIGN KEY (`quoted_in`) REFERENCES `epcs` (`id`);
+ALTER TABLE `epcs_quotes`
+ADD CONSTRAINT `epcs_quotes_ibfk_2` FOREIGN KEY (`quote_id`) REFERENCES `quotes` (`id`),
+ADD CONSTRAINT `epcs_quotes_ibfk_1` FOREIGN KEY (`epc_id`) REFERENCES `epcs` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
